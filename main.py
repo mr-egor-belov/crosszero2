5c7f1f04a0c6a57e1d5340cc821b8bc24ba2d81b
mr-egor-belov <egorbelov@me.com>
2025-01-31 15:01:00 +0300

    add new code for pole

---
+++ main.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("crosszero")
+window.geometry("300x350")
+
+current_player = "X"
+buttons = []
+
+
+def check_winner():
+   for i in range(3):
+       if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
+           return True
+       if buttons[0][i]["text"] == buttons[1][i]["text"] == buttons[2][i]["text"] != "":
+           return True
+
+   if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+       return True
+   if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+       return True
+
+   return False
+
+
+def on_click(row, col):
+   global current_player
+
+   if buttons[row][col]['text'] != "":
+       return
+
+   buttons[row][col]['text'] = current_player
+
+   if check_winner():
+       messagebox.showinfo("Игра окончена",f"Игрок {current_player} победил!")
+
+   current_player = "0" if current_player == "X" else "X"
+
+
+for i in range(3):
+   row = []
+   for j in range(3):
+       btn = tk.Button(window, text="", font=("Arial", 20), width=5, height=2, command=lambda r=i, c=j: on_click(r, c))
+       btn.grid(row=i, column=j)
+       row.append(btn)
+   buttons.append(row)
+
+
+window.mainloop()
